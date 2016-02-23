043d0a9317778e928ee5453f3b63d70cefa37ac3
KAI <a0983481006@gmail.com>
2016-02-23 20:28:51 +0800

    刪除密碼

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 $db_host = 'localhost';
 $db_user = 'a0983481006@gmail';
-$db_pass = 's1040323';
+$db_pass = '';
 $db_name = 'test';
 ?>
--- test.php
+++ test.php
@@ -1,5 +1,5 @@
 <?php
 include('incsession.php');
 
-echo '這是會員區';
+echo '這是會員區！';
 ?>
